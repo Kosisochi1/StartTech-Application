ab96abbe3415721fd472d4d11cde002283e9a5aa
Kosisochi1 <emmanuelezeoyirikosi@yahoo.com>
2026-01-25 22:08:34 +0100

    feat/added deploy folder

--- starttech-application/Client/src/lib/apiClient.ts
+++ starttech-application/Client/src/lib/apiClient.ts
@@ -1,8 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const apiClient = axios.create({
-    baseURL: API_BASE_URL,
-    withCredentials: true, // Crucial for httpOnly cookies
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  }, // Crucial for httpOnly cookies
 });
